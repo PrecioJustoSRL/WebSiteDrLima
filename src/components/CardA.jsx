89be5edd5b1e15815439f10d4aece2427ce77387
raulDeDevWorld <proyection009@gmail.com>
2024-01-21 22:42:25 -0400

    DC

--- src/components/CardA.jsx
+++ src/components/CardA.jsx
@@ -8,7 +8,7 @@ export default function Card({i}) {
         <div className="w-full max-w-sm  rounded-lg  mt-5">
         
             <div className="w-full flex flex-col items-center ">
-                <img className="w-[100px] h-[100px] mb-3 rounded-full shadow-lg" src={i.url} alt="Bonnie image" />
+                <img className="w-[150px] h-[150px] mb-3 rounded-full shadow-lg" src={i.url} alt="Bonnie image" />
                 <h5 className="mb-1 text-xl font-medium text-gray-900 ">{i.perfil}</h5>
                 {/* <span className="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span> */}
                 <p className="w-full text-gray-950 text-center italic p-5" style={{ background: 'linear-gradient(to left, #CFE0E8, #ffffff)' }}>"{i.testimonio}"</p>
